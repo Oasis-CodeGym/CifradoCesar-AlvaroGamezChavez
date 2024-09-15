1ae6d3efe1a4451fd5ec88b8097880a6af688b4e
Alvaro Gamez Chavez <gaositd@gmail.com>
2024-09-15 17:03:57 -0600

    Modifing Cipher class

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,6 +1,10 @@
 package src;
 
 public class Cipher {
+  private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+      'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+      'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
   private char[] alphabet;
   public Cipher(char[] alphabet) {
     this.alphabet = alphabet;
